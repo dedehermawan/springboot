b75f537b0748465a790412588c9e670bfc3f45e7
dedehermawan <alidefit@gmail.com>
2016-07-23 01:23:50 +0700

    template springboot + bootstrap

---
+++ src/test/java/com/dedehermawan/belajar/CrudspringbootApplicationTests.java
@@ -0,0 +1,18 @@
+package com.dedehermawan.belajar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CrudspringbootApplication.class)
+@WebAppConfiguration
+public class CrudspringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
